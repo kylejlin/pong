56d1fc5aadba5e4dfe4b6c80a34752f09762ac47
Kyle Lin <kylejlin.us@gmail.com>
2017-10-11 21:29:07 -0700

    Change logo to match style of favicon

--- src/PongHomeMenu.js
+++ src/PongHomeMenu.js
@@ -5,12 +5,7 @@ import './PongHomeMenu.css';
 function PongHomeMenu({ onStartButtonClicked }) {
   return (
     <div className="PongHomeMenu">
-      <div className="PongHomeMenu-title">
-        <span className="PongHomeMenu-title-P">P</span>
-        <span className="PongHomeMenu-title-o">o</span>
-        <span className="PongHomeMenu-title-n">n</span>
-        <span className="PongHomeMenu-title-g">g</span>
-      </div>
+      <img src="/homescreen-logo.svg" alt="Pong" className="PongHomeMenu-logo" />
 
       <PongButton onClick={onStartButtonClicked}>
         Play
